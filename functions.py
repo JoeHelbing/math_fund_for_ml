45c562c90c58ee3eda6bc09b26891de9febd5dcb
JoeHelbing <helbing@uchicago.edu>
2022-11-23 16:03:51 -0600

    Updated with GPS bin crawl though outcomes not correct

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
 
 class LogisticRegression:
@@ -14,8 +16,8 @@ class LogisticRegression:
         self.n_iters = n_iters
         self.weights = None
         self.bias = None
-        self.loss = []
-
+        self.plot_loss = plot_loss
+        self.cost = []
 
     def train(self, X, y):
         """
@@ -25,13 +27,11 @@ class LogisticRegression:
         self.weights, self.bias = self.initialize_weights(num_features)
         self.gradient_descent(X, y, num_samples)
 
-
     def sigmoid_function(self, x):
         """
         Compute sigmoid function
         """
         return 1 / (1 + np.exp(-x))
-
 
     def initialize_weights(self, dimensions):
         """
@@ -41,13 +41,11 @@ class LogisticRegression:
         bias = 0
         return weights, bias
 
-
     def linear_function(self, X):
         """
         Compute the linear function
         """
         return np.dot(X, self.weights) + self.bias
-
 
     def update_weights(self, gradient, dbias):
         """
@@ -55,7 +53,6 @@ class LogisticRegression:
         """
         self.weights -= self.lr * gradient
         self.bias -= self.lr * dbias
-
 
     def gradient_descent(self, X, y, num_samples):
         """
@@ -67,23 +64,23 @@ class LogisticRegression:
             gradient = (1 / num_samples) * np.dot(X.T, (y_pred - y))
             dbias = (1 / num_samples) * np.sum(y_pred - y)
             self.update_weights(gradient, dbias)
-            loss = self.logistic_regression_loss_function(y, y_pred)
-            self.loss.append(loss)
-            if loss < 0.00001:
+            cost = self.logistic_regression_cost_function(y, y_pred)
+            self.cost.append(cost)
+            if cost < 0.00001:
                 print("Converged at iteration {}".format(_))
                 break
-        
+
         if self.plot_loss:
             self.plot_loss()
-    
 
-    def logistic_regression_loss_function(self, y, y_pred):
+    def logistic_regression_cost_function(self, y, y_pred):
         """
         Compute the loss of the model
         """
-        loss = np.sum(-(y * np.log(y_pred) - (1 - y) * np.log(1 - y_pred)))
-        return loss
-        
+        cost = -(1 / len(y)) * np.sum(
+            (y * np.log(y_pred) - (1 - y) * np.log(1 - y_pred))
+        )
+        return cost
 
     def predict(self, X):
         """
@@ -93,7 +90,6 @@ class LogisticRegression:
         sigmoid = self.sigmoid_function(linear)
         y_pred = np.round(sigmoid)
         return y_pred
-    
 
     def accuracy(self, y_true, y_pred):
         """
@@ -106,7 +102,7 @@ class LogisticRegression:
         """
         Plot the loss of the model
         """
-        plt.plot(self.loss)
+        plt.plot(self.cost)
         plt.xlabel("Iterations")
         plt.ylabel("Loss")
         plt.show()
@@ -141,3 +137,38 @@ def dummy_data(df, number_vertical_bins, number_horizontal_bins):
     df = bin_gps(df, number_vertical_bins, number_horizontal_bins)
     df = pd.get_dummies(df, columns=["LAT_BIN", "LON_BIN"])
     return df
+
+
+def gps_crawl(crash_df):
+    """
+    crawls across a bin structure and creates a new dataframe
+    """
+    y = crash_df.DAMAGE
+    X = crash_df.drop("DAMAGE", axis=1)
+
+    # remove all columns not LATITUDE or LONGITUDE
+    X = X[["LATITUDE", "LONGITUDE"]]
+
+    for bins in range(1, 501, 50):
+        print("=========================================")
+        print(f"Number of vertical bins: {bins}")
+        print(f"Number of horizontal bins: {501 - bins}")
+        # bin the GPS data
+        binned_df = dummy_data(X, bins, 501 - bins)
+
+        # remove original LATITUDE and LONGITUDE columns
+        binned_df.drop(["LATITUDE", "LONGITUDE"], axis=1, inplace=True)
+        print(f"Binned dataframe shape: {binned_df.shape}")
+        # split data into train and test sets
+        X_train, X_test, y_train, y_test = train_test_split(
+            binned_df, y, test_size=0.9, random_state=42
+        )
+        X_train.shape, X_test.shape, y_train.shape, y_test.shape
+        model = LogisticRegression(n_iters=100)
+        model.train(X_train, y_train)
+        y_pred = model.predict(X_test)
+        new_accuracy = model.accuracy(y_test, y_pred)
+        print("Accuracy: ", new_accuracy)
+        if bins != 1:
+            print(f"Accuracy Change: {accuracy - new_accuracy}")
+        accuracy = new_accuracy
